19278e5fa50296b7ff1e81b5ef879dee4844546b
WON <aekik1118@naver.com>
2019-02-12 15:57:49 +0900

    test 수정 및 파일 업로드 부분 주석처리

--- src/main/java/com/coffeeshop/service/BoardServiceImpl.java
+++ src/main/java/com/coffeeshop/service/BoardServiceImpl.java
@@ -55,15 +55,15 @@ public class BoardServiceImpl implements BoardService {
 	public boolean modify(BoardVO board) {
 		log.info("modify.." + board);
 		
-		attachMapper.deleteAll(board.getBno());
+		//attachMapper.deleteAll(board.getBno());
 		boolean modifyResult = mapper.update(board) == 1;
 		
-		if(modifyResult && board.getAttachList() != null  && board.getAttachList().size() > 0) {
+		/*if(modifyResult && board.getAttachList() != null  && board.getAttachList().size() > 0) {
 			board.getAttachList().forEach(attach->{
 				attach.setBno(board.getBno());
 				attachMapper.insert(attach);
 			});
-		}
+		}*/
 		
 		return modifyResult;
 	}
--- src/test/java/com/coffeeshop/mapper/BoardMapperTests.java
+++ src/test/java/com/coffeeshop/mapper/BoardMapperTests.java
@@ -1,6 +1,7 @@
 package com.coffeeshop.mapper;
 
 import com.coffeeshop.config.RootConfig;
+import com.coffeeshop.domain.BoardVO;
 import com.coffeeshop.domain.Criteria;
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
@@ -9,7 +10,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
+import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {RootConfig.class})
@@ -34,6 +35,20 @@ public class BoardMapperTests {
         int count = mapper.getTotalCount(new Criteria());
 
         log.info(count);
+
+        BoardVO boardVO = mapper.read(1L);
+
+        log.info(boardVO);
+
+        mapper.delete(1L);
+
+        assertEquals(count-1,mapper.getTotalCount(new Criteria()));
+
+        mapper.insert(boardVO);
+
+        assertEquals(count,mapper.getTotalCount(new Criteria()));
+
+
     }
 
 
