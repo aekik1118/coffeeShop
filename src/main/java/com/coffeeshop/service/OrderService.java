c3aa619ee05753f77cde157a3b2f7a3c7a7ab59e
bnzn <tjdwls0607@naver.com>
2019-02-17 15:12:18 +0900

    orderService 기본 추문 처리

---
+++ src/main/java/com/coffeeshop/service/OrderService.java
@@ -0,0 +1,15 @@
+package com.coffeeshop.service;
+
+import com.coffeeshop.domain.OrderVO;
+import com.coffeeshop.domain.OrderedProductVO;
+
+import java.util.List;
+
+public interface OrderService {
+
+    public void regist(OrderVO orderVO, OrderedProductVO orderedPVO);
+    public List<OrderVO> getWating();
+    public boolean complete(String customer);
+    public boolean cancle(String customer);
+
+}
---
+++ src/main/java/com/coffeeshop/service/OrderServiceImpl.java
@@ -0,0 +1,60 @@
+package com.coffeeshop.service;
+
+import com.coffeeshop.domain.OrderVO;
+import com.coffeeshop.domain.OrderedProductVO;
+import com.coffeeshop.mapper.OrderMapper;
+import com.coffeeshop.mapper.OrderedProductMapper;
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Log4j
+@Service
+public class OrderServiceImpl implements OrderService{
+
+
+    @Setter(onMethod_ = {@Autowired})
+    private OrderMapper mapper;
+
+    @Setter(onMethod_ = {@Autowired})
+    private OrderedProductMapper orderedPMapper;
+
+    @Override
+    public void regist(OrderVO orderVO, OrderedProductVO orderedPVO) {
+        log.info("regist.......... :" + orderVO +"  " + orderedPVO);
+        mapper.insert(orderVO);
+        orderedPMapper.insert(orderedPVO);
+
+        int amount = 0;
+        int price = 0;
+        for(OrderedProductVO vo : orderedPMapper.select(orderVO.getOno())) {
+            amount += vo.getCount();
+            price += vo.getCount() + vo.getPrice();
+        }
+
+        mapper.insertAmount(amount);
+        mapper.insertPrice(price);
+    }
+
+    @Override
+    public List<OrderVO> getWating() {
+
+        log.info("Wating List.......... :" + mapper.watingList());
+        return mapper.watingList();
+    }
+
+    @Override
+    public boolean complete(String customer) {
+
+        return mapper.setSold(customer);
+    }
+
+    @Override
+    public boolean cancle(String customer) {
+
+        return false;
+    }
+}
