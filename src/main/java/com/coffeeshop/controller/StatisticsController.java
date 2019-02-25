5dd3d0973e51e6e42fb540b9b24379b8000358ff
bnzn <tjdwls0607@naver.com>
2019-02-26 00:32:47 +0900

    컨트롤러 기본

--- src/main/java/com/coffeeshop/controller/StatisticsController.java
+++ src/main/java/com/coffeeshop/controller/StatisticsController.java
@@ -6,10 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,12 +19,13 @@ public class StatisticsController {
     private StatisticsService service;
 
     @GetMapping("/chart")
-    public void chart(){
+    public @ResponseBody List<StatisticsDTO> chart(){
         log.info("chart(get)...........");
+        return service.getStatisticsList("2019-01-25");
     }
 
     @PostMapping("/chart")
-    public List<StatisticsDTO> chart(@RequestParam("startDate") String startDate){
+    public @ResponseBody List<StatisticsDTO> chart(@RequestParam("startDate") String startDate){
 
         log.info("chart(post)..........");
         return service.getStatisticsList(startDate);
